78ea74015790dc513988bfed3280309d2a5e4d73
Roman Hordiichuk <roman.hordiichuk@sombrainc.com>
2023-10-06 15:32:53 +0300

    changes [06.10.2023]

--- script.js
+++ script.js
@@ -2,19 +2,33 @@ const headerTimeEl = document.getElementById('header-time');
 const submitNewWordButton = document.getElementById('submit-new-word');
 const newWordField = document.getElementById('words-input');
 const wordsListEl = document.getElementById('words-list');
-const testArr = [
+const testWordsArr = [
     { id: 1, word: 'narrow', dateAdded: '20/09/2023' },
     { id: 2, word: 'seamlessly', dateAdded: '21/09/2023' },
     { id: 3, word: 'go forth', dateAdded: '22/09/2023' },
     { id: 4, word: 'strum', dateAdded: '23/09/2023' },
     { id: 5, word: 'bid you adieu', dateAdded: '23/09/2023' }
 ];
+const testTranscriptionArr = [
+    { id: 1, word: "|ˈnærəʊ|", dateAdded: '20/09/2023' },
+    { id: 2, word: "test2", dateAdded: '21/09/2023' },
+    { id: 3, word: "test3", dateAdded: '22/09/2023' },
+    { id: 4, word: "test4", dateAdded: '23/09/2023' },
+    { id: 5, word: "test5", dateAdded: '23/09/2023' }
+];
+const testTranslateArr = [
+    { id: 1, translation: '', dateAdded: '20/09/2023' },
+    { id: 2, translation: 'seamlessly', dateAdded: '21/09/2023' },
+    { id: 3, translation: 'go forth', dateAdded: '22/09/2023' },
+    { id: 4, translation: 'strum', dateAdded: '23/09/2023' },
+    { id: 5, translation: 'bid you adieu', dateAdded: '23/09/2023' }
+];
 
 
 submitNewWordButton.addEventListener("click", () => {
     if (newWordField.value !== '') {
         const currentDate = new Date().toLocaleDateString();
-        testArr.push({ id: null, word: newWordField.value, dateAdded: currentDate });
+        testWordsArr.push({ id: null, word: newWordField.value, dateAdded: currentDate });
 
         wordsListEl.innerHTML += `<li>${newWordField.value}</li>`;
         newWordField.value = '';
@@ -34,7 +48,7 @@ setCurrentTime();
 function renderWords() {
     wordsListEl.innerHTML = '';
 
-    for (el of testArr) {
+    for (el of testWordsArr) {
         wordsListEl.innerHTML += `<li>${el.word}</li>`;
     }
 }
